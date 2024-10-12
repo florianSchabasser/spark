5d14626342654b9ff51ed247e78cb2943da514a8
Florian Schabasser <florian.schabasser@hotmail.de>
2024-10-12 06:04:27 +0000

    (1) fix eval-examples start.sh;

--- eval_examples/src/main/scala/org/apache/spark/start.sh
+++ eval_examples/src/main/scala/org/apache/spark/start.sh
@@ -1,10 +1,11 @@
 docker exec -it spark-master-lineage spark-submit \
   --class WordCountLineage \
-  --master spark://spark-master:7077 \
+  --master spark://spark-master-lineage:7077 \
   --conf "spark.rdd.lineage.detailed=false" \
   hdfs://namenode:9000/user/root/eval_examples-3.5.3.jar 10MB.txt 10MB.txt
 
 docker exec -it spark-master spark-submit \
   --class WordCount \
-  --master spark://spark-master:6077 \
+  --master spark://spark-master:7077 \
   hdfs://namenode:9000/user/root/eval_examples-3.5.3.jar 10MB.txt 10MB.txt
+
